4bb974593abc97b34670876c4400340bf84e6fc9
Kevin L. Keys <klkeysb@gmail.com>
2019-05-20 14:12:28 -0700

    test commit porting simulation jobs to SGE environment

--- analyses/03_hapmap3-simulation/src/02_test_prediction_models/test_prediction_models.sh
+++ analyses/03_hapmap3-simulation/src/02_test_prediction_models/test_prediction_models.sh
@@ -12,7 +12,7 @@
 # -- "model_sizes" controls the number(s) of causal eQTLs to include in simulated models
 # -- "props" controls the proportions of shared eQTLs between all populations
 #
-# Other variables (nfolds_*, nthreads) are crossvalidation parameters fed to R. 
+# Other variables (nfolds_*, nthreads) are crossvalidation parameters fed to R.
 # ==========================================================================================
 
 # ==========================================================================================
@@ -27,11 +27,11 @@ set -u  ## script will exit if it sees an uninitialized variable
 # binaries
 # ==========================================================================================
 PYTHON2=$(whereis python2.7 | awk '{print $2}') ## default: system Python v2.7
-RSCRIPT=$(whereis Rscript | awk '{print $2}') ## will default to system Rscript, if installed 
+RSCRIPT=$(whereis Rscript | awk '{print $2}') ## will default to system Rscript, if installed
 
 
 # ==========================================================================================
-# directories, filepaths 
+# directories, filepaths
 # ==========================================================================================
 thisdir="$(dirname $(readlink -f $0))"
 analysisdir="${thisdir}/../../analysis"
@@ -46,10 +46,11 @@ genelist="${datadir}/chr22.genelist.txt"
 
 
 # ==========================================================================================
-# external scripts 
+# external scripts
 # ==========================================================================================
 PYTHON_multithread_commands="${thisdir}/multithread_commands.py"
 R_simulate_crosspop_pred="${thisdir}/simulate_crosspopulation_prediction.R"
+BASH_qsub_template="${thisdir}/qsub_template.sh"
 
 
 # ==========================================================================================
@@ -59,7 +60,7 @@ same_effects="TRUE"
 
 # these parameters control the nested crossvalidation
 # in general: more $nfolds_external or $nfolds_internal --> more compututational burden
-# but more $nfolds_parallel --> more parallel throughput 
+# but more $nfolds_parallel --> more parallel throughput
 nfolds_external=5
 nfolds_internal=10
 nfolds_parallel=1
@@ -78,7 +79,7 @@ nthreads=24  ## this is fed to the PYTHON script to run $NTHREADS jobs in parall
 
 
 # ==========================================================================================
-# executable code 
+# executable code
 # ==========================================================================================
 
 # make simulation output directory, if it doesn't exist
@@ -105,13 +106,13 @@ for gene in ${genenames[@]}; do
     ceu_file="${genotypedir}/CEU.${gene}.chr22.raw"
     yri_file="${genotypedir}/YRI.${gene}.chr22.raw"
 
-    # guard against possible missing genotype files here 
-    if [[ -e "${aa_file}" ]]; then 
+    # guard against possible missing genotype files here
+    if [[ -e "${aa_file}" ]]; then
 
         # loop over all random seeds
         for seed in ${seeds[@]}; do
 
-            # first run cases where proportions vary 
+            # first run cases where proportions vary
             same_eqtls="FALSE"
             for prop in ${props[@]}; do
                 for k in ${model_sizes[@]}; do
@@ -121,7 +122,7 @@ for gene in ${genenames[@]}; do
                     nohup_err="${output_text_dir}/${gene}.model-${same_eqtls}.effects-${same_effects}.k-${k}.seed-${seed}.prop-${prop}.err"
 
                     # write the actual command as echo, append to job list
-                    echo "$RSCRIPT $R_simulate_crosspop_pred --gene-name \"${gene}\" --genotypes-pop1 \"${ceu_file}\" --genotypes-pop2 \"${yri_file}\" --genotypes-admix \"${aa_file}\" --output-directory \"${output_data_dir}\" --same-eqtls ${same_eqtls} --same-eqtl-effects ${same_effects} --nfolds-internal ${nfolds_internal} --nfolds-external ${nfolds_external} --nfolds-parallel ${nfolds_parallel} --num-eqtls ${k} --random-seed ${seed} --fraction-overlapping-eqtls ${prop} > ${nohup_out} 2> ${nohup_err}" >> ${simulation_joblist} 
+                    echo "$RSCRIPT $R_simulate_crosspop_pred --gene-name \"${gene}\" --genotypes-pop1 \"${ceu_file}\" --genotypes-pop2 \"${yri_file}\" --genotypes-admix \"${aa_file}\" --output-directory \"${output_data_dir}\" --same-eqtls ${same_eqtls} --same-eqtl-effects ${same_effects} --nfolds-internal ${nfolds_internal} --nfolds-external ${nfolds_external} --nfolds-parallel ${nfolds_parallel} --num-eqtls ${k} --random-seed ${seed} --fraction-overlapping-eqtls ${prop} > ${nohup_out} 2> ${nohup_err}" >> ${simulation_joblist}
                 done
             done
 
@@ -131,13 +132,33 @@ for gene in ${genenames[@]}; do
             for k in ${model_sizes[@]}; do
                 nohup_out="${output_text_dir}/${gene}.model-${same_eqtls}.effects-${same_effects}.k-${k}.seed-${seed}.prop-${prop}.out"
                 nohup_err="${output_text_dir}/${gene}.model-${same_eqtls}.effects-${same_effects}.k-${k}.seed-${seed}.prop-${prop}.err"
-                echo "$RSCRIPT $R_simulate_crosspop_pred --gene-name \"${gene}\" --genotypes-pop1 \"${ceu_file}\" --genotypes-pop2 \"${yri_file}\" --genotypes-admix \"${aa_file}\" --output-directory \"${output_data_dir}\" --same-eqtls ${same_eqtls} --same-eqtl-effects ${same_effects} --nfolds-internal ${nfolds_internal} --nfolds-external ${nfolds_external} --nfolds-parallel ${nfolds_parallel} --num-eqtls ${k} --random-seed ${seed} --fraction-overlapping-eqtls ${prop} > ${nohup_out} 2> ${nohup_err}" >> ${simulation_joblist} 
+                echo "$RSCRIPT $R_simulate_crosspop_pred --gene-name \"${gene}\" --genotypes-pop1 \"${ceu_file}\" --genotypes-pop2 \"${yri_file}\" --genotypes-admix \"${aa_file}\" --output-directory \"${output_data_dir}\" --same-eqtls ${same_eqtls} --same-eqtl-effects ${same_effects} --nfolds-internal ${nfolds_internal} --nfolds-external ${nfolds_external} --nfolds-parallel ${nfolds_parallel} --num-eqtls ${k} --random-seed ${seed} --fraction-overlapping-eqtls ${prop} > ${nohup_out} 2> ${nohup_err}" >> ${simulation_joblist}
             done
         done
     fi
 done
 
-# run model train-test-validate
-# this will schedule a Python process that will manage the threads
-# think of it as a light (and brittle and fragile) SGE engine
-nohup $PYTHON2 $PYTHON_multithread_commands --file ${simulation_joblist} --jobs $nthreads > ${outputdir}/nohup.multithread.out 2> ${outputdir}/nohup.multithread.err &
+## this code works for running model train-test-validate on a single computer or server
+## this will schedule a Python process that will manage the threads
+## think of it as a light (and brittle and fragile) SGE engine
+#nohup $PYTHON2 $PYTHON_multithread_commands --file ${simulation_joblist} --jobs $nthreads > ${outputdir}/nohup.multithread.out 2> ${outputdir}/nohup.multithread.err &
+
+# need total number of jobs
+njobs=$(wc -l < ${simulation_joblist})
+
+# set SGE variables
+h_rt="23:59:59"
+scratch_memory="1G"
+memory_limit="1G"
+logdir="${output_text_dir}"
+
+# execute with SGE framework
+qsub -N "1kg.sim.expression" \
+     -v "command_file=${simulation_joblist}" \
+     -t 1-${njobs} \
+     -e "${logdir}" \
+     -o "${logdir}" \
+     -l mem_free="${memory_limit}" \
+     -l scratch="${scratch_memory}" \
+     -l h_rt="${h_rt}" \
+     ${BASH_qsub_template}
