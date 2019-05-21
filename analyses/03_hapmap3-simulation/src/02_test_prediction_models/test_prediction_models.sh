b6d881773db3e8c59394c7cf6c544d3ef1a67a2b
Kevin L. Keys <klkeysb@gmail.com>
2019-05-21 11:58:08 -0700

    estimate prediction models with varying admix proportions on 1 server

--- analyses/03_hapmap3-simulation/src/02_test_prediction_models/test_prediction_models.sh
+++ analyses/03_hapmap3-simulation/src/02_test_prediction_models/test_prediction_models.sh
@@ -106,59 +106,87 @@ for gene in ${genenames[@]}; do
     ceu_file="${genotypedir}/CEU.${gene}.chr22.raw"
     yri_file="${genotypedir}/YRI.${gene}.chr22.raw"
 
-    # guard against possible missing genotype files here
-    if [[ -e "${aa_file}" ]]; then
+#    # guard against possible missing genotype files here
+#    if [[ -e "${aa_file}" ]]; then
+#
+#        # loop over all random seeds
+#        for seed in ${seeds[@]}; do
+#
+#            # first run cases where proportions vary
+#            same_eqtls="FALSE"
+#            for prop in ${props[@]}; do
+#                for k in ${model_sizes[@]}; do
+#
+#                    # output files for nohup
+#                    nohup_out="${output_text_dir}/${gene}.model-${same_eqtls}.effects-${same_effects}.k-${k}.seed-${seed}.prop-${prop}.out"
+#                    nohup_err="${output_text_dir}/${gene}.model-${same_eqtls}.effects-${same_effects}.k-${k}.seed-${seed}.prop-${prop}.err"
+#
+#                    # write the actual command as echo, append to job list
+#                    echo "$RSCRIPT $R_simulate_crosspop_pred --gene-name \"${gene}\" --genotypes-pop1 \"${ceu_file}\" --genotypes-pop2 \"${yri_file}\" --genotypes-admix \"${aa_file}\" --output-directory \"${output_data_dir}\" --same-eqtls ${same_eqtls} --same-eqtl-effects ${same_effects} --nfolds-internal ${nfolds_internal} --nfolds-external ${nfolds_external} --nfolds-parallel ${nfolds_parallel} --num-eqtls ${k} --random-seed ${seed} --fraction-overlapping-eqtls ${prop} > ${nohup_out} 2> ${nohup_err}" >> ${simulation_joblist}
+#                done
+#            done
+#
+#            # now do case where all pops share eQTLs
+#            same_eqtls="TRUE"
+#            prop="1.0" ## this is merely naming placeholder, it has no function when same_eqtls = TRUE
+#            for k in ${model_sizes[@]}; do
+#                nohup_out="${output_text_dir}/${gene}.model-${same_eqtls}.effects-${same_effects}.k-${k}.seed-${seed}.prop-${prop}.out"
+#                nohup_err="${output_text_dir}/${gene}.model-${same_eqtls}.effects-${same_effects}.k-${k}.seed-${seed}.prop-${prop}.err"
+#                echo "$RSCRIPT $R_simulate_crosspop_pred --gene-name \"${gene}\" --genotypes-pop1 \"${ceu_file}\" --genotypes-pop2 \"${yri_file}\" --genotypes-admix \"${aa_file}\" --output-directory \"${output_data_dir}\" --same-eqtls ${same_eqtls} --same-eqtl-effects ${same_effects} --nfolds-internal ${nfolds_internal} --nfolds-external ${nfolds_external} --nfolds-parallel ${nfolds_parallel} --num-eqtls ${k} --random-seed ${seed} --fraction-overlapping-eqtls ${prop} > ${nohup_out} 2> ${nohup_err}" >> ${simulation_joblist}
+#            done
+#        done
+#    fi
+
+    # now do cases where admixture proportion is varied
+    # the AA file name changes by admix prop
+    # thus, must loop through these separate from the previous two cases
+    same_eqtls="FALSE"
+    prop="0.5"
+    k=10
+    CEU_props=("0" "0.1" "0.2" "0.3" "0.4" "0.5" "0.6" "0.7" "0.8" "0.9" "1") ## <-- note that 0 is NOT 0.0, 1 is NOT 1.0 !!!
+    YRI_props=("1" "0.9" "0.8" "0.7" "0.6" "0.5" "0.4" "0.3" "0.2" "0.1" "0") ## blame R for printing double as integer in analysis step 1
+
+    # as before, assuming that #${CEU_props[@}} == #${YRI_props[@}} here...
+    for i in ${!CEU_props[@]}; do
+
+        CEU_prop=${CEU_props[$i]}
+        YRI_prop=${YRI_props[$i]}
+        aa_file="${genotypedir}/AA.${gene}_CEU${CEU_prop}_YRI${YRI_prop}.chr22.raw"
+
+        if [[ -e "${aa_file}" ]]; then
 
         # loop over all random seeds
         for seed in ${seeds[@]}; do
-
-            # first run cases where proportions vary
-            same_eqtls="FALSE"
-            for prop in ${props[@]}; do
-                for k in ${model_sizes[@]}; do
-
-                    # output files for nohup
-                    nohup_out="${output_text_dir}/${gene}.model-${same_eqtls}.effects-${same_effects}.k-${k}.seed-${seed}.prop-${prop}.out"
-                    nohup_err="${output_text_dir}/${gene}.model-${same_eqtls}.effects-${same_effects}.k-${k}.seed-${seed}.prop-${prop}.err"
-
-                    # write the actual command as echo, append to job list
-                    echo "$RSCRIPT $R_simulate_crosspop_pred --gene-name \"${gene}\" --genotypes-pop1 \"${ceu_file}\" --genotypes-pop2 \"${yri_file}\" --genotypes-admix \"${aa_file}\" --output-directory \"${output_data_dir}\" --same-eqtls ${same_eqtls} --same-eqtl-effects ${same_effects} --nfolds-internal ${nfolds_internal} --nfolds-external ${nfolds_external} --nfolds-parallel ${nfolds_parallel} --num-eqtls ${k} --random-seed ${seed} --fraction-overlapping-eqtls ${prop} > ${nohup_out} 2> ${nohup_err}" >> ${simulation_joblist}
-                done
-            done
-
-            # now do case where all pops share eQTLs
-            same_eqtls="TRUE"
-            prop="1.0" ## this is merely naming placeholder, it has no function when same_eqtls = TRUE
-            for k in ${model_sizes[@]}; do
-                nohup_out="${output_text_dir}/${gene}.model-${same_eqtls}.effects-${same_effects}.k-${k}.seed-${seed}.prop-${prop}.out"
-                nohup_err="${output_text_dir}/${gene}.model-${same_eqtls}.effects-${same_effects}.k-${k}.seed-${seed}.prop-${prop}.err"
-                echo "$RSCRIPT $R_simulate_crosspop_pred --gene-name \"${gene}\" --genotypes-pop1 \"${ceu_file}\" --genotypes-pop2 \"${yri_file}\" --genotypes-admix \"${aa_file}\" --output-directory \"${output_data_dir}\" --same-eqtls ${same_eqtls} --same-eqtl-effects ${same_effects} --nfolds-internal ${nfolds_internal} --nfolds-external ${nfolds_external} --nfolds-parallel ${nfolds_parallel} --num-eqtls ${k} --random-seed ${seed} --fraction-overlapping-eqtls ${prop} > ${nohup_out} 2> ${nohup_err}" >> ${simulation_joblist}
-            done
+                nohup_out="${output_text_dir}/${gene}.model-${same_eqtls}.effects-${same_effects}.k-${k}.prop-${prop}.CEU-${CEU_prop}.YRI-${YRI_prop}.seed-${seed}.out"
+                nohup_err="${output_text_dir}/${gene}.model-${same_eqtls}.effects-${same_effects}.k-${k}.prop-${prop}.CEU-${CEU_prop}.YRI-${YRI_prop}.seed-${seed}.err"
+                echo "$RSCRIPT $R_simulate_crosspop_pred --gene-name \"${gene}\" --genotypes-pop1 \"${ceu_file}\" --genotypes-pop2 \"${yri_file}\" --genotypes-admix \"${aa_file}\" --output-directory \"${output_data_dir}\" --same-eqtls ${same_eqtls} --same-eqtl-effects ${same_effects} --nfolds-internal ${nfolds_internal} --nfolds-external ${nfolds_external} --nfolds-parallel ${nfolds_parallel} --num-eqtls ${k} --random-seed ${seed} --fraction-overlapping-eqtls ${prop} --admix-proportion-pop1 ${CEU_prop} --admix-proportion-pop2 ${YRI_prop} > ${nohup_out} 2> ${nohup_err}" >> ${simulation_joblist}
         done
-    fi
+
+        fi
+    done
 done
 
 ## this code works for running model train-test-validate on a single computer or server
 ## this will schedule a Python process that will manage the threads
 ## think of it as a light (and brittle and fragile) SGE engine
-#nohup $PYTHON2 $PYTHON_multithread_commands --file ${simulation_joblist} --jobs $nthreads > ${outputdir}/nohup.multithread.out 2> ${outputdir}/nohup.multithread.err &
+nohup $PYTHON2 $PYTHON_multithread_commands --file ${simulation_joblist} --jobs $nthreads > ${outputdir}/nohup.multithread.out 2> ${outputdir}/nohup.multithread.err &
 
-# need total number of jobs
-njobs=$(wc -l < ${simulation_joblist})
-
-# set SGE variables
-h_rt="23:59:59"
-scratch_memory="1G"
-memory_limit="1G"
-logdir="${output_text_dir}"
-
-# execute with SGE framework
-qsub -N "1kg.sim.expression" \
-     -v "command_file=${simulation_joblist}" \
-     -t 1-${njobs} \
-     -e "${logdir}" \
-     -o "${logdir}" \
-     -l mem_free="${memory_limit}" \
-     -l scratch="${scratch_memory}" \
-     -l h_rt="${h_rt}" \
-     ${BASH_qsub_template}
+## need total number of jobs
+#njobs=$(wc -l < ${simulation_joblist})
+#
+## set SGE variables
+#h_rt="23:59:59"
+#scratch_memory="1G"
+#memory_limit="1G"
+#logdir="${output_text_dir}"
+#
+## execute with SGE framework
+#qsub -N "1kg.sim.expression" \
+#     -v "command_file=${simulation_joblist}" \
+#     -t 1-${njobs} \
+#     -e "${logdir}" \
+#     -o "${logdir}" \
+#     -l mem_free="${memory_limit}" \
+#     -l scratch="${scratch_memory}" \
+#     -l h_rt="${h_rt}" \
+#     ${BASH_qsub_template}
